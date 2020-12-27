8bf16e7c36e8e85ef7e473e68c086a1df8fba547
vimukthi <vimukthi@efuturesworld.com>
2020-12-27 23:11:52 +0530

    add survey

--- app/Http/Controllers/Api/QuestionController.php
+++ app/Http/Controllers/Api/QuestionController.php
@@ -26,7 +26,7 @@ class QuestionController extends Controller
             return response()->json([
                 "status"=>1,
                 "data"=>[
-                    "questions"=>$questions
+                    "surveys"=>$questions
                 ]
             ]);
 
